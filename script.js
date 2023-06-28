b6e91fec42b7ecadb2ad339a29dce18440fc8ef4
Joshuathomas22135 <134388878+Joshuathomas22135@users.noreply.github.com>
2023-06-28 17:07:54 +0400

    Finalized Project Files

---
+++ script.js
@@ -0,0 +1,13 @@
+const date = document.getElementById('date');
+const day = document.getElementById('day');
+const month = document.getElementById('month');
+const year = document.getElementById('year');
+
+const today = new Date();
+
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wenesday", "Thursday", "Friday", "Saturday"];
+const AllManths = ["January" , "February", "March", "April", "May", "June", "July", "August", "September", "October", "November" , "December"]
+date.innerHTML = (today.getDate() <10?"0":"") +today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = AllManths[today.getMonth()];
+year.innerHTML = today.getFullYear()
